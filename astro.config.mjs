1e58fe5b01f3f207875b90f2ca43a6b78f8f20e0
Schedrin Vladimir <chafiliny@gmail.com>
2024-06-03 17:38:11 +0600

    add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://endearing-dango-b17c53.netlify.app",
+  integrations: [preact()]
 });
