51bc40a59ba6495a9f6323c06d245c8c23d811b9
Schedrin Vladimir <chafiliny@gmail.com>
2024-06-03 17:27:11 +0600

    add tags and rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://endearing-dango-b17c53.netlify.app",
+});
