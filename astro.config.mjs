40e9f9cf455eaa92eafb4ff81c259979c3a17e10
Schedrin Vladimir <chafiliny@gmail.com>
2024-06-06 02:21:30 +0600

    remove tutorial styles tags and preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://endearing-dango-b17c53.netlify.app",
-  integrations: [preact()]
 });
+
